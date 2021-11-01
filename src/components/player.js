e61c7cb383cd260d9bfa13768787fb7c191b5eb0
tp <dev@zheteng.cloud>
2021-11-01 21:20:55 +0800

    Correct typo of label

--- src/components/player.js
+++ src/components/player.js
@@ -5,11 +5,11 @@ import Slider from "@mui/material/Slider"
 const marks = [
   {
     value: 0,
-    lable: "0:00",
+    label: "0:00",
   },
   {
     value: 100,
-    lable: "2:00",
+    label: "2:00",
   },
 ]
 
@@ -17,11 +17,10 @@ const Player = ({ progress, isPlaying }) => (
   <Box sx={{ width: "auto" }}>
     <Slider
       aria-label="audio progress"
-      defaultValue={80}
       step={1}
       value={progress}
       marks={marks}
-      valueLabelDisplay="on"
+      valueLabelDisplay="auto"
     />
   </Box>
 )
