be5cfc9b7221c10b7ca0f29717dd2deb5e6d737c
tp <dev@zheteng.cloud>
2021-11-01 12:19:30 +0800

    Add more storybook controls

--- src/stories/Player.stories.js
+++ src/stories/Player.stories.js
@@ -4,10 +4,19 @@ import Player from "../components/player"
 export default {
   component: Player,
   title: "Components/Player",
+  argTypes: {
+    progress: {
+      options: [1,100,2],
+      control: {
+        type: 'range'
+      }
+    }
+  }
 }
 
 const Template = args => <Player {...args} />
 export const Primary = Template.bind({})
 Primary.args = {
   isPlaying: true,
+  progress: 20
 }
