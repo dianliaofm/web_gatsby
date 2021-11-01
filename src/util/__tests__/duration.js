9e30679b674365f07d9eaebdc9cd0306036fb8ca
tp <dev@zheteng.cloud>
2021-11-01 23:27:07 +0800

    format duration func

--- src/util/__tests__/duration.js
+++ src/util/__tests__/duration.js
@@ -1,9 +1,32 @@
-import { formatDuration } from '../duration'
+import { formatDuration } from "../duration"
 
-describe('Duration', () => {
-    it("format", () => {
-        let sec = 60 * 60
-        let res = formatDuration(sec)
-        expect(res).toEqual(60)
-    })
+describe("Duration", () => {
+  it("format", () => {
+    const s50 = 50
+    const res = formatDuration(s50)
+    expect(res).toEqual("00:50")
+    const min1 = 60
+    const res2 = formatDuration(min1)
+    expect(res2).toEqual("01:00")
+
+    const m2s16 = 2 * 60 + 16
+    const res3 = formatDuration(m2s16)
+    expect(res3).toEqual("02:16")
+
+    const m59s21 = 59 * 60 + 21
+    const res4 = formatDuration(m59s21)
+    expect(res4).toEqual("59:21")
+
+    const hr1 = 60 * 60
+    const res5 = formatDuration(hr1)
+    expect(res5).toEqual("01:00:00")
+
+    const hr2 = 63 * 60 + 37
+    const res6 = formatDuration(hr2)
+    expect(res6).toEqual("01:03:37")
+
+    const hr3 = 2 * 60 * 60 + 47 * 60 + 1
+    const res7 = formatDuration(hr3)
+    expect(res7).toEqual("02:47:01")
+  })
 })
--- src/util/duration.js
+++ src/util/duration.js
@@ -1,8 +1,22 @@
-
-const formatDuration = (seconds) => {
-    return seconds / 60
+/**
+ * format seconds to padded string
+ * < 1h: 00:40, 20:31, 59:11
+ * >= 1h: 01:00:12, 02:28:44
+ * @param {integer} seconds
+ * @returns string
+ */
+const formatDuration = seconds => {
+  const s = seconds % 60
+  const mins = (seconds - s) / 60
+  if (mins < 60) {
+    return `${pad(mins)}:${pad(s)}`
+  } else {
+    const min_remainder = mins % 60
+    const hrs = (mins - min_remainder) / 60
+    return `${pad(hrs)}:${pad(min_remainder)}:${pad(s)}`
+  }
 }
 
-export {
-    formatDuration
-}
+const pad = x => (x > 9 ? `${x}` : `0${x}`)
+
+export { formatDuration }
