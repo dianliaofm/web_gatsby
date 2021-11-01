d00b4729e2c5b2f1fb1c44a98fe8f464f7933402
tp <dev@zheteng.cloud>
2021-11-01 20:10:56 +0800

    Setup mui

--- src/components/player.js
+++ src/components/player.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from "react"
+import Box from "@mui/material/Box"
+import Slider from "@mui/material/Slider"
+
+const marks = [{
+    value: 0,
+    lable: "0:00"
+},{
+    value: 100,
+    lable: "2:00"
+}
+]
 
 const Player = () => (
-    <div>media player</div>
+  <Box sx={{ width: 300 }}>
+    <Slider
+      aria-label="audio progress"
+      defaultValue={80}
+      getAriaValueText={v => `${v}tt`}
+      step={10}
+      marks={marks}
+      valueLabelDisplay="on"
+    />
+  </Box>
 )
-
 
 export default Player
