85d045a66350fc1b3f5323b25fc34d939bd00fba
tp <dev@zheteng.cloud>
2021-11-05 15:05:04 +0800

    Add audiocontrol

--- src/components/htmlAudio.js
+++ src/components/htmlAudio.js
@@ -47,9 +47,60 @@ const Audio = () => {
         <h2>
           {epId} {title}
         </h2>
+        <AudioControls
+          isPlaying={isPlaying}
+          onPrevClick={toPrevTrack}
+          onNextClick={toNextTrack}
+          onPlayPauseClick={setIsPlaying}
+        />
       </div>
     </div>
   )
 }
 
 export default Audio
+
+const AudioControls = ({
+  onPrevClick,
+  isPlaying,
+  onPlayPauseClick,
+  onNextClick,
+}) => (
+  <div className="audio-controls">
+    <button
+      type="button"
+      className="prev"
+      aria-label="Previous"
+      onClick={onPrevClick}
+    >
+      Prev
+    </button>
+    {isPlaying ? (
+      <button
+        type="button"
+        className="pause"
+        onClick={() => onPlayPauseClick(false)}
+        aria-label="Pause"
+      >
+        Pause
+      </button>
+    ) : (
+      <button
+        type="button"
+        className="play"
+        onClick={() => onPlayPauseClick(true)}
+        aria-label="Play"
+      >
+        Play
+      </button>
+    )}
+    <button
+      type="button"
+      className="next"
+      aria-label="Next"
+      onClick={onNextClick}
+    >
+      Next
+    </button>
+  </div>
+)
