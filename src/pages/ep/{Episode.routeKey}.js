87b2441e53add83571bd255f4106fafff8ce4db6
tp <dev@zheteng.cloud>
2021-11-07 23:17:49 +0800

    Add query in ep pages

--- src/pages/ep/{Episode.routeKey}.js
+++ src/pages/ep/{Episode.routeKey}.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import { graphql } from "gatsby"
+import React from "react"
+import App from "../../components/app"
 
-const Episode =(props) => {
-    return (
-        <div>
-            episode id:{props.pageContext.id}, route key {props.params.routeKey}
-        </div>
-    )
+const Episode = props => {
+  const { title, image, url } = props.data.episode
+  return (
+    <App>
+      <div>
+        episode title:{title}, route key {props.params.routeKey}
+      </div>
+      <div>image: {image}</div>
+      <div>url: {url}</div>
+    </App>
+  )
 }
 
 export default Episode
+
+export const query = graphql`
+  query ($id: String) {
+    episode(id: { eq: $id }) {
+      title
+      image
+      url
+    }
+  }
+`
