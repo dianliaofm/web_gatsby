42a7da9c6b37f15d652839b8475ab84c1aeae79f
tp <dev@zheteng.cloud>
2021-11-16 12:45:29 +0800

    Add topbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,13 +5,18 @@ import "@fontsource/roboto/400.css"
 import "@fontsource/roboto/500.css"
 import "@fontsource/roboto/700.css"
 import "./layout.css"
+import AppBar from "@mui/material/AppBar"
+import Box from "@mui/material/Box"
+import Toolbar from "@mui/material/Toolbar"
+import Typography from "@mui/material/Typography"
+import IconButton from "@mui/material/IconButton"
+import MenuIcon from "@mui/icons-material/Menu"
+import Button from "@mui/material/Button"
 
 const CustomLayout = ({ children, player }) => {
   return (
     <>
-      <div>
-        <Link to="/">Home</Link>
-      </div>
+      <Topbar />
       {children}
       {player}
     </>
@@ -19,3 +24,36 @@ const CustomLayout = ({ children, player }) => {
 }
 
 export default CustomLayout
+
+function Topbar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Button>
+              <Link
+                to="/"
+                style={{
+                  textDecoration: "none",
+                  color: "white"
+                }}
+              >
+                Dianliao
+              </Link>
+            </Button>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
